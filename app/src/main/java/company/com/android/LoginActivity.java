6e49b835fe868213604094f197a2dc236c6a8ea0
phongnguyen <trungphong1325@gmail.com>
2018-10-15 23:37:42 +0700

    implement login

---
+++ app/src/main/java/company/com/android/LoginActivity.java
@@ -0,0 +1,13 @@
+package company.com.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
